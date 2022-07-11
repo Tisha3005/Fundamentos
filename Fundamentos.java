4e19fac9f7eedf592a54d3dc830f387dfee46f79
Tisha3005 <ribeiro_thais@hotmail.com>
2022-07-11 18:34:11 -0300

    Fundamentos
    Classes criadas em aula VaiNaWeb

---
+++ Fundamentos.java
@@ -0,0 +1,14 @@
+package Fundamentos;
+
+public class Fundamentos {
+	public static void main(String[] args) {
+		
+		//CONCATENAR
+		System.out.println("Sextou!! bora" + "curtir o final de semana");
+		
+		//IMPRIMIR
+		System.out.println("ESCOVAR OS DENTES");
+	}
+		
+
+}
---
+++ VariaveisEConstantes.java
@@ -0,0 +1,36 @@
+package Fundamentos;
+
+// aula dia 11/07/22
+
+public class VariaveisEConstantes {
+	
+	public static void main(String[] args) {
+		
+		//area da circunferencia
+		//raio * raio * pi;
+		int raio = 3;
+		double pi = 3.14159;
+		System.out.println(3*3*3.14159);
+		System.out.println(raio*raio*pi);
+		
+	System.out.println();
+		
+			//podemos reutilizar as informacoes ao inves de ficar alterando
+		int primeiroNumero = 9;
+		int segundoNumero = 3;
+		
+		System.out.println(primeiroNumero*segundoNumero);
+		System.out.println(primeiroNumero/segundoNumero);	
+		System.out.println(primeiroNumero+segundoNumero);	
+		System.out.println(primeiroNumero-segundoNumero);	
+		
+	System.out.println();
+		
+		System.out.println(9*3);	
+		System.out.println(9/3);
+		System.out.println(9+3);
+		System.out.println(9-3);
+	}
+	
+
+}
